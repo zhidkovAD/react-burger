b422091623bba4e4c88172755ca47161c59b42a3
Zhidkov Andrew <andrej-zhidkov@mail.ru>
2025-06-23 19:08:53 +0300

    стилистические правки

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -4,16 +4,16 @@ import styles from './profile.module.css';
 
 function Profile() {
     return (
-        <main className="page-container page-container-profile">
-            <div className={`page-container-profile-wrapper ${styles.profile}`}>
-            <nav className="page-container-profile-sidebar ml-5 mr-15">
-                <ul>
-                    <li>
+        <main className={`page-container page-container-profile ${styles.main_profile}`}>
+            <div className={`page-container-profile-wrapper ${styles.wrap_profile}`}>
+            <nav className={`page-container-profile-sidebar ml-5 mr-15 ${styles.navBar}`}>
+                <ul className={styles.list_menu}>
+                    <li className={styles.li_profile}>
                         <NavLink to="" end>{({ isActive }) => (
                             <span className={`text text_type_main-medium ${isActive ? "text_color_primary" : "text_color_inactive"}`}>Профиль</span>
                         )}</NavLink>
                     </li>
-                    <li>
+                    <li className={styles.li_profile}>
                         <NavLink to={"orders"}>{({ isActive }) => (
                             <span className={`text text_type_main-medium ${isActive ? "text_color_primary" : "text_color_inactive"}`}>История заказов</span>
                         )}</NavLink>
