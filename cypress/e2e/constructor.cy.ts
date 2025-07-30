43555e9bf2c3a9d09bb7b401d5a83937ab12b9dc
zhidkovAD <andrej-zhidkov@mail.ru>
2025-07-30 18:45:45 +0300

    Исправления после ревью

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -2,30 +2,38 @@
 
 describe('constuctor page', () => {
     beforeEach(() => {
-        cy.visit('http://localhost:5173');
+        // baseUrl внутри cypress.config.ts
+        cy.visit('/');
     });
+
+    const ingredient_card_selector = '[class^=cy_test_card_ingredient]'
+    const modal_dialog_selector = '[class^=modal_dialog]'
+    const modal_close_selector = '[class^=modal_close]'
+    const email = "test1997@test.ru"
+    const password = "test12345"
+    const name_zone_add_bun = 'Добавьте булку'
+    const name_zone_add_ingredient = 'Добавьте ингридиенты'
 
     it('show ingredient detail', () => {
         cy.contains('Конструктор');
         cy.contains('Соберите бургер');
-        cy.get('[class^=cy_test_card_ingredient]').first().click();
-        cy.get('[class^=modal_dialog]').contains('Детали ингредиента');
-        cy.get('[class^=modal_close]').click();
-        cy.get('[class^=modal_dialog]').should('not.exist');
+        cy.get(ingredient_card_selector).first().click();
+        cy.get(modal_dialog_selector).contains('Детали ингредиента');
+        cy.get(modal_close_selector).click();
+        cy.get(modal_dialog_selector).should('not.exist');
     });
 
     it('create order', () => {
         cy.get('[class^=cy_test_ingredients_category]').as('list');
-        cy.get('@list').eq(0).find('[class^=cy_test_card_ingredient]').first().as('bun');
-        cy.get('@list').eq(1).find('[class^=cy_test_card_ingredient]').first().as('ingredient');
+        cy.get('@list').eq(0).find(ingredient_card_selector).first().as('bun');
+        cy.get('@list').eq(1).find(ingredient_card_selector).first().as('ingredient');
         cy.get('[class^=cy_constructor_test').first().as('dest');
         cy.get('@dest').children().first().as('bun-dest')
         cy.get('@bun-dest').next().as('ingredient-dest');
         cy.contains('Оформить заказ').as('order-button')
-        // cy.get('[class^=burger-constructor-order_total] button').as('order-button');
 
-        cy.contains('Добавьте булку');
-        cy.contains('Добавьте ингридиенты');
+        cy.contains(name_zone_add_bun);
+        cy.contains(name_zone_add_ingredient);
 
         cy.get('@bun').trigger('dragstart');
         cy.get('@bun-dest').trigger('drop');
@@ -34,15 +42,15 @@ describe('constuctor page', () => {
         cy.get('@order-button').click();
 
         cy.contains('Вход');
-        cy.get('[name=email]').type("test1997@test.ru");
-        cy.get('[name=password]').type("test12345");
+        cy.get('[name=email]').type(email);
+        cy.get('[name=password]').type(password);
         cy.contains('button', 'Войти').click();
 
         cy.get('@order-button', { timeout: 20000 }).trigger('click');
         cy.get('[class^=cy_order_number]', { timeout: 40000 }).contains(/\d+/);
-        cy.get('[class^=modal_close]').click();
+        cy.get(modal_close_selector).click();
 
-        cy.contains('Добавьте булку');
-        cy.contains('Добавьте ингридиенты');
+        cy.contains(name_zone_add_bun);
+        cy.contains(name_zone_add_ingredient);
     });
 })
